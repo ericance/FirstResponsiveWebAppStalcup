af7ef1424d8e77651a868dd81f0c1c4992eafb91
Eric <ericstalcup@gmail.com>
2021-02-02 09:49:05 -0600

    Add project files.

---
+++ FirstResponsiveWebAppStalcup/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FirstResponsiveWebAppStalcup.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
