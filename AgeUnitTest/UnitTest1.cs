9084fa77d9b432905a243aeb19f03b2cc7bea402
Eric <ericstalcup@gmail.com>
2021-02-12 23:53:43 -0600

    UnitTestingNote

---
+++ AgeUnitTest/UnitTest1.cs
@@ -0,0 +1,27 @@
+using System;
+using Xunit;
+
+using FirstResponsiveWebAppStalcup;
+using FirstResponsiveWebAppStalcup.Models;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Web;
+using Microsoft.AspNetCore.Mvc;
+
+using FirstResponsiveWebAppStalcup.Controllers;
+
+namespace AgeUnitTest
+{
+	public class UnitTest1
+	{
+		[Fact]
+		public void Test1()
+		{
+			/* I have no idea how to make Unit Tests work with
+			 * ASP.NET and MVC.
+			 * Read the email I sent you explaining in greater detail.
+			 */
+		}
+	}
+}
